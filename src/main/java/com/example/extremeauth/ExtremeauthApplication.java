37d8a710cb014d9a2b53c2e0e1f4dcb9ff5a5b9f
yi yang <yiyang@thoughtworks.com>
2020-04-08 18:45:03 +0800

    init

---
+++ src/main/java/com/example/extremeauth/ExtremeauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.extremeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremeauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremeauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/extremeauth/ExtremeauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extremeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtremeauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
